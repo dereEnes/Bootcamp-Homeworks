0fb01a32c7c9b51e89d605754ab739fa91fa1723
dereEnes <dere.enes01@gmail.com>
2022-03-26 21:50:46 +0300

    IUserService Added to business abstracts

---
+++ Homework-3/First.App/First.App.Core/Abstract/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using First.App.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace First.App.Business.Abstract
+{
+    public interface IUserService
+    {
+        User GetCompany(Expression<Func<User, bool>> filter);
+        List<User> GetAllCompany();
+        void AddCompany(User company);
+        void UpdateCompany(User company);
+        void DeleteCompany(User company);
+    }
+}
